db369868f4e0995658e7358fa9d7e6c6ec44a295
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2024-03-03 15:31:16 +0100

    Kod początkowy z błędami

---
+++ Snake/Obstakel.cs
@@ -0,0 +1,14 @@
+﻿namespace Snake
+{
+    public class Obstakel
+
+    {
+        public int Xpos { get; set; }
+
+        // ?
+
+        public ConsoleColor schermKleur { get; set; }
+
+        public string karacter { get; set; }
+    }
+}
